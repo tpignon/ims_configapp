b278c3bfabb2974eff1eceaf88be9f4f96184ecb
Mathieu Pignon <mathieu.pignon@pit-business.com>
2017-05-29 17:05:46 +0200

    Server LDAP added First Controller WinAD managed to connect to Windows AD

---
+++ qms/src/AppBundle/Controller/WinADController.php
@@ -0,0 +1,46 @@
+<?php
+// src/AppBundle/Controller/WinADController.php
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+//use Symfony\Component\Ldap\LdapClient;
+use Symfony\Component\Ldap\Ldap;
+
+class WinADController
+{
+    /**
+     * @Route("/winad/connect")
+     */
+    public function connectAction()
+    {
+      $res = "null";
+
+      // Eléments d'authentification LDAP
+      $ldaprdn  = 'INTERNAL\MPignon';     // DN ou RDN LDAP
+      $ldappass = 'FOR:pitip123';  // Mot de passe associé
+
+      // Connexion au serveur LDAP
+      $ldapconn = ldap_connect("internal.imsglobal.com")
+          or die("Impossible de se connecter au serveur LDAP.");
+
+      if ($ldapconn) {
+          // Connexion au serveur LDAP
+          $ldapbind = ldap_bind($ldapconn, $ldaprdn, $ldappass);
+
+          // Vérification de l'authentification
+          if ($ldapbind) {
+              $res = "passed";
+          } else {
+            $res = "failed";
+          }
+
+      }
+
+        return new Response(
+            "<html><body>Connection $res.</body></html>"
+        );
+    }
+}
+?>
