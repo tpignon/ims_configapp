5ecbb84395de7f1e43ee46262c7a2226e4dc83d7
Mathieu Pignon <mathieu.pignon@pit-business.com>
2017-05-30 18:33:56 +0200

    Security via InMemory ou Standalone AD, le service AD ne fonctionne pas

--- qms/src/AppBundle/Controller/LuckyController.php
+++ qms/src/AppBundle/Controller/LuckyController.php
@@ -5,6 +5,8 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+
 class LuckyController
 {
     /**
@@ -12,10 +14,26 @@ class LuckyController
      */
     public function numberAction()
     {
+        // SECURITY: The second parameter is used to specify on what object the role is tested.
+        $this->denyAccessUnlessGranted('ROLE_USER', $this, 'Unable to access this page!');
+
         $number = mt_rand(0, 100);
 
         return new Response(
-            '<html><body>Lucky number: '.$number.'</body></html>'
+            '<html><body>Lucky number 1: '.$number.'</body></html>'
+        );
+    }
+
+    /**
+     * @Route("/lucky/number2")
+     * @Security("has_role('ROLE_USER')")
+     */
+    public function number2Action()
+    {
+        $number = mt_rand(0, 100);
+
+        return new Response(
+            '<html><body>Lucky number 2: '.$number.'</body></html>'
         );
     }
 }
